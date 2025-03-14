0dc7e0034daa8ffe6253230fb35cb3f552a06627
Martina <martugurevich@hotmail.com>
2025-03-14 04:19:26 +0000

    ejercicio 2a terminado

---
+++ ejercicio_2/ej2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+using namespace std;
+
+//creo un archivo.  //dsp hago un switch.
+
+enum leyendas { DEBUG=1, INFO, WARNING, ERROR, CRITICAL };
+
+constexpr array<const char*,5> leyendasStrings = {
+    "DEBUG",
+    "INFO",
+    "WARNING", 
+    "ERROR", 
+    "CRITICAL"
+};
+
+//que el usuario elija la opcion
+void opciones_severidad(){
+    //usuario elige el nivel de severidad
+    cout<<"Opciones del NIvel de Severidad:" << "\n";
+    for(size_t i=0; i<leyendasStrings.size(); i++){
+        cout<< i+1 <<"-"<< leyendasStrings [i]<< "\n";
+    }
+
+}
+
+void logMessage(string mensaje, int NivelSeveridad){
+    //usuario elige la opcion
+    //lo que se seleccione se agrega al archivo log
+    ofstream logfile("Log.txt");
+    if (!logfile){
+        cerr<<"nos se puede abrir el archivo. \n";
+        return;
+    }
+    
+    string nivel;
+    switch (NivelSeveridad)
+    {
+        case DEBUG:
+            nivel="DEBUG";
+            break;
+        case INFO:
+            nivel="INFO";
+            break;
+        case WARNING:
+            nivel="WARNING";
+            break;
+        case ERROR:
+            nivel="ERROR";
+            break;
+        case CRITICAL:
+            nivel="CRITICAL";
+            break;
+        default:
+        cerr<< "NO es valido este nivel de severidad";
+            return;
+    }
+    logfile<<"[" <<nivel<< "]"<< mensaje <<"\n";
+    logfile.close();
+}
+
+int main(){
+    
+
+    int seleccion;
+    char mensaje[256];
+    opciones_severidad();
+
+    cout<<"seleccione nivel de severidad: ";
+    scanf("%d", &seleccion);
+
+    while(seleccion< 1 || seleccion> 5){
+        cout<< "opcion invalida, intente denuevo:" ;//poner que intente nuevamente
+        scanf("%d", &seleccion);
+    }
+
+    cout<< "ingrese su mensaje; ";
+    scanf(" %[^\n]", mensaje);// ver que onda esto si lo puedo dejar asi para evadir el error
+
+    logMessage(mensaje, seleccion);
+
+    return 0;
+}
+
+
